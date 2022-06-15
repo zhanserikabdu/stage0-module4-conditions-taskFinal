2aa3d5e45c2dc23e8b813a135959f01ef83dd15f
Oleg Zheleznov <oleg-zheleznov@mail.ru>
2022-06-15 18:46:33 +0300

    Fix tests

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,11 +2,5 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
-        first = first ^ second;
-        second = first  ^ second;
-        first = first  ^ second;
-        System.out.println(first);
-
-        System.out.println(second);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++
@@ -1,7 +0,0 @@
-package school.mjc.stage0.conditions.finalTask;
-
-public class TriangleSidesValidator {
-    public void validate(double firstSide, double secondSide, double thirdSide) {
-
-    }
-}
