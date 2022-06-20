7cafbb01f5facbcbb1bd3ae8edd32a4ff6b4d695
Andrekukshinov <56270371+Andrekukshinov@users.noreply.github.com>
2022-06-21 01:28:01 +0200

    TriangleSidesValidator

---
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -0,0 +1,7 @@
+package school.mjc.stage0.conditions.finalTask;
+
+public class TriangleSidesValidator {
+    public void validate(double firstSide, double secondSide, double thirdSide) {
+
+    }
+}
