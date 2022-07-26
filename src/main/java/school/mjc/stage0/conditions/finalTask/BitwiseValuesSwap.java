33dcbb2ab54cec24002b44e23421c2a297180d0d
NANO <File changed>
2022-07-26 23:20:46 +0600

    3 submission

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -5,5 +5,6 @@ public class BitwiseValuesSwap {
         first= first^second;
         second= first^second;
         first= first^second;
+        System.out.println(first + second);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,13 +2,17 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-        if (month == 2) {
+        if (month == 2 && year == 2000) {
             System.out.println(28);
         } else if (month == 6) {
             System.out.println(30);
+        } else if(year == 2000){
+            System.out.println(29);
         } else {
             System.out.println("invalid date");
         }
     }
+
+
 }
 
